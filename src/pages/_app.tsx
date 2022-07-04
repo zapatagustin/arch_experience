c04dff624676fc694313c08664fca4d35fb1eb4c
zapatagustin <zapatagustin4@gmail.com>
2022-07-04 17:39:10 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,38 @@
+import Head from 'next/head';
+import React, { useEffect } from 'react';
+import { Layout } from '../components/layout';
+import '../styles/global.css';
+import { ShellProvider } from '../utils/shellProvider';
+import { ThemeProvider } from '../utils/themeProvider';
+
+const App = ({ Component, pageProps }) => {
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  const onClickAnywhere = () => {
+    inputRef.current.focus();
+  };
+
+  useEffect(() => {
+    localStorage.setItem('visitedAt', new Date().toString());
+  }, []);
+
+  return (
+    <ThemeProvider>
+      <ShellProvider>
+        <Head>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+            key="viewport"
+          />
+        </Head>
+
+        <Layout onClick={onClickAnywhere}>
+          <Component {...pageProps} inputRef={inputRef} />
+        </Layout>
+      </ShellProvider>
+    </ThemeProvider>
+  );
+};
+
+export default App;
